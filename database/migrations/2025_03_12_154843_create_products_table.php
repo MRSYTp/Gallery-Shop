5ad74627a362f3ba968ec9a6fbe4917320ddb60b
MohammadReza Salehi <mo.salehi1387p@gmail.com>
2025-03-12 19:41:11 +0330

    created all tables

---
+++ database/migrations/2025_03_12_154843_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('owner_id');
+            $table->char('title', 255);
+            $table->text('description');
+            $table->char('thumbnail_url');
+            $table->char('demo_url');
+            $table->char('source_url');
+            $table->unsignedInteger('price');
+            $table->timestamps();
+            
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('owner_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
